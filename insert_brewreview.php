7ab179fb57a2a6387d3352065dc752f568edb290
Brian Swift <swiftbw@gmail.com>
2018-09-30 18:42:58 -0500

    further updates

--- insert_brewreview.php
+++ insert_brewreview.php
@@ -1,9 +1,28 @@
- <?php
-  if($_POST['submit'] == "Submit")
-  {
-    $brewname = $_POST['brewname'];
-  }
-?>
+<?php
+if($_POST['submit'] == "Submit")
+{
+	$errorMessage = "";
+	inputs = array("reviewdate", "brewname", "brewery", "venue", "vessel", "color", "mouthfeel", "taste", "aroma", "comment", "overall");
+
+	for ( $array as $item ) {
+	if(empty($_POST[$item]))
+	{
+		$errorMessage .= "<li>You forgot to enter " . $item . "!</li>";
+	}
+	
+	$varMovie = $_POST['formMovie'];
+	$varName = $_POST['formName'];
+
+	if(empty($errorMessage)) 
+	{
+		$fs = fopen("mydata.csv","a");
+		fwrite($fs,$varName . ", " . $varMovie . "\n");
+		fclose($fs);
+		
+		header("Location: thankyou.html");
+		exit;
+	}
+}
 
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html> <head>
@@ -14,6 +33,14 @@
 <h1>
 Testing 123
 </h1>
+	<?php
+		if(!empty($errorMessage)) 
+		{
+			echo("<p>There was an error with your form:</p>\n");
+			echo("<ul>" . $errorMessage . "</ul>\n");
+		} 
+	?>
+
 <form action="insert_brewreview.php" method="post">
 Review Date: <input type="date" name="reviewdate"><br>
 Brew Name:   <input type="text" name="brewname"><br>
@@ -26,9 +53,13 @@ Taste:       <input type="date" name="taste"><br>
 Aroma:       <input type="text" name="aroma"><br>
 Comments:    <input type="text" name="comment"><br>
 Overall:     <input type="number" name="overall"><br>
-Submit:      <input type="submit"
+Submit:      <input type="submit" name="submit" value="Submit"><br>
 </form>
 <hr>
 <address></address>
 <!-- hhmts start -->Last modified: Sun Sep 30 14:31:08 CDT 2018 <!-- hhmts end -->
 </body> </html>
+
+
+
+
